67a23a4265dd83fd6b072f9f29eda9714599c6f5
TheeGhoost <93801929+TheeGhoost@users.noreply.github.com>
2022-09-17 13:53:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,4 +20,35 @@
     });
 })(document);
 
+/* 
+////////////////////////////////////////////////////////////
+                                    CONTACT
+////////////////////////////////////////////////////////////
+*/
 
+document.querySelector("#input-name").addEventListener("keyup", e => {
+    let $inputName = document.querySelector("#input-name");
+    if ($inputName.value.length != 0) {
+        $inputName.classList.add("text-valid");
+    } else {
+        $inputName.classList.remove("text-valid");
+    }
+});
+
+document.querySelector("#input-email").addEventListener("keyup", e => {
+    let $inputEmail = document.querySelector("#input-email");
+    if ($inputEmail.value.length != 0) {
+        $inputEmail.classList.add("text-valid");
+    } else {
+        $inputEmail.classList.remove("text-valid");
+    }
+});
+
+document.querySelector("#textarea-coments").addEventListener("keyup", e => {
+    let $textarea = document.querySelector("#textarea-coments");
+    if ($textarea.value.length != 0) {
+        $textarea.classList.add("textarea-valid");
+    } else {
+        $textarea.classList.remove("textarea-valid");
+    }
+});
